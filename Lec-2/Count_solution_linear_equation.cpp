1fd9de4227ae5919568388647bb2d89b179f8b48
vuanhtuan1407 <vuanhtuan1407@gmail.com>
2022-12-26 23:51:06 +0700

    lec-2/count..

--- Lec-2/Count_solution_linear_equation.cpp
+++ Lec-2/Count_solution_linear_equation.cpp
@@ -3,46 +3,45 @@
 
 using namespace std;
 
-int n, m;
+int n, M;
 int a[N];
 int x[N];
 int T = 0;
-int S[N];
+int a_min = 0;
+// int S[N];
 int count_solution = 0;
 
-void countSolution(){
-    if(T == m) count_solution += 1;
+int findMin() {
+    int min = a[1];
+    for(int i=2; i<=n; i++){
+        if(min > a[i]) min = a[i];
+    }
+    return min;
 }
 
-// void Try(int k){
-//     for(int v=1; v<=S[k-1]; v++){
-//         x[k] = v;
-//         T += a[k]*x[k];
-//         S[k] = (m-T)/((n-k+1)*a[k]);
-//         if(k == n) countSolution();
-//         else {
-//             Try(k+1);
-//             T -= a[k]*x[k];
-//         }
-//     }
-// }
+void countSolution(){
+    // if(T == M) count_solution += 1;
+    if (T == M){
+        // for(int i=1; i<=n; i++) cout << x[i] << ' ';
+        // cout << endl;
+        count_solution += 1;
+    }
+}
 
 void Try(int k){
-    for(int i=1; i<=m; i++){
-        x[k] = i;
+    for(int v=1; v<=(M-T-a_min*(n-k-1))/a[k]; v++){
+        x[k] = v;
         T += a[k]*x[k];
         if(k == n) countSolution();
-        else {
-            Try(k+1);
-            T -= a[k]*x[k];
-        }
+        else Try(k+1);
+        T -= a[k]*x[k];
     }
 }
 
 int main() {
-    cin >> n >> m;
+    cin >> n >> M;
     for(int i=1; i<=n; i++) cin >> a[i];
-    // S[0] = m/(n*a[1]);
+    a_min = findMin();
     Try(1);
     cout << count_solution;
     return 0;
